3c21b68bf0f00c2b22a834ed1dccd3b4e0135bbf
TestoKlaus <colin.hassenbach@gmail.com>
2025-12-27 00:18:29 +0100

    feat: Update documentation for PCA saturation functions to include sample proportion and normalization options

--- R/pca_saturation.R
+++ R/pca_saturation.R
@@ -20,8 +20,10 @@
 #' @param n_cores Number of cores for parallel processing. Default: NULL (uses all available - 1)
 #'
 #' @return A list with two elements:
+#' \describe{
 #'   \item{saturation_data}{Data frame with columns: sample_size, metric_type, mean, median, sd, q025, q975, sample_proportion. Metrics represent morphospace coverage (range/volume).}
 #'   \item{parameters}{List of analysis parameters used}
+#' }
 #'
 #' @export
 #'
